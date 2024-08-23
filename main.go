c50b4ccefd15ee180d0d5f70510ec4dc864acd2e
akakimbe <akakimbe@noreply.platform.alem.school>
2024-08-23 15:31:56 +0000

    Update main.go

--- main.go
+++ main.go
@@ -11,15 +11,24 @@ var (
 	defaultNumber = flag.Int("default number", 100, "100")
 	help          = flag.Bool("help", false, "Da")
 	prefix        = flag.Int("default prefix", 2, "2")
+	// wordStart     = flag.String("start word", "Chapter", "First")
 )
 
 func main() {
+	prefixDivide := *prefix - 1
+	var allSave []string
+	var fileSave []string
 	var save_data []string
 	var save_txt []string
+	var save_word []string
+	var save_prefix []string
 	var randomCount int = 0
+	var save_identically_word int = 0
+	var save_txt_part []string
 	fileName := "the_great_gatsby.txt"
-	fileSave := fileRead(fileName)
+
 	save_data = append(save_data, os.Args...)
+	fileSave = fileRead(fileName)
 	if len(save_data) >= 3 {
 		if save_data[1] == "-w" {
 			number := string_to_int(save_data[2])
@@ -32,10 +41,9 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Incorrect input")
 			os.Exit(1)
 		}
-
 		if *prefix > 0 {
 			for i := 0; i < *prefix; i++ {
-				save_txt = append(save_txt, fileSave[i])
+				save_prefix = append(save_prefix, fileSave[i])
 			}
 		} else if *prefix == 0 {
 			fmt.Fprintln(os.Stderr, "Prefix can't be equal to 0")
@@ -44,10 +52,9 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Prefix can't be less than 0")
 			os.Exit(1)
 		}
-		for i := 0; i < len(save_txt); i++ {
-			fmt.Printf(save_txt[i])
+		for i := 0; i < len(save_prefix); i++ {
+			allSave = append(allSave, save_prefix[i])
 		}
-		fmt.Printf(" ")
 		wordMore := ""
 		for {
 			_, err := fmt.Fscan(os.Stdin, &wordMore)
@@ -56,18 +63,38 @@ func main() {
 			}
 			save_txt = append(save_txt, wordMore)
 		}
-		inFileRandom := rand.Intn(len(save_txt))
-		for i := inFileRandom; i < len(save_txt); i++ {
-			fmt.Print(save_txt[i], " ")
-			randomCount++
-			if save_txt[i] == "." {
-				randomCount--
-			}
-			if randomCount == *defaultNumber-*prefix {
-				fmt.Println()
-				os.Exit(0)
+		// save_txt_copy := save_txt
+		for i := 0; i < len(save_txt); i++ {
+			save_txt_part = append(save_txt_part, save_txt[i])
+			if len(save_txt_part) == *prefix {
+				for j := 0; j < *prefix; j++ {
+					if save_prefix[j] == save_txt_part[j] {
+						save_identically_word++
+					}
+					if save_identically_word == *prefix {
+						if i+1 < len(save_txt) {
+							save_word = append(save_word, save_txt[i+1])
+							save_prefix = save_prefix[*prefix-prefixDivide:]
+							for l := 0; l < len(save_word); l++ {
+								save_prefix = append(save_prefix, save_word[l])
+								allSave = append(allSave, save_word...)
+								fmt.Println(save_prefix)
+							}
+						}
+						i = 0
+					}
+				}
+				save_identically_word = 0
+				save_word = []string{}
+				save_txt_part = []string{}
 			}
 		}
+		fmt.Println(save_word)
+		for i := 0; i < len(allSave); i++ {
+			fmt.Print(allSave[i])
+			fmt.Print(" ")
+		}
+		fmt.Println()
 	} else {
 		if len(save_data) >= 2 {
 			if save_data[1] == "--help" {
