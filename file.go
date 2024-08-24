6a5911369139b289edd01eb190404c2c6c6c0c1f
akakimbe <akakimbe@noreply.platform.alem.school>
2024-08-24 14:55:00 +0000

    Add file.go

---
+++ file.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func fullFile() []string{
+	var txt_words []string
+	wordMore := ""
+	for {
+		_, err := fmt.Fscan(os.Stdin, &wordMore)
+		if err != nil {
+			break
+		}
+		txt_words = append(txt_words, wordMore)
+	}
+	return txt_words
+}
