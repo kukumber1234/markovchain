3d97e8c7f1a219ea67dab084b7b7224864f4fec2
akakimbe <akakimbe@noreply.platform.alem.school>
2024-08-25 12:03:16 +0000

    Update map.go

--- map.go
+++ map.go
@@ -3,47 +3,53 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"os"
 	"strings"
 )
 
 func mapSave(save_txt []string, fileSave []string) {
 	wordMap := make(map[string][]string)
-	var save_identically_word int = 0
+	var check_identically_word bool
 	var save_txt_part []string
 	var randWordChoose int
 
-	for i := 0; i < len(fileSave); i++ {
-		fmt.Print(fileSave[i] + " ")
+	if *defaultNumber < len(fileSave) {
+		for i := 0; i < *defaultNumber; i++ {
+			fmt.Print(fileSave[i] + " ")
+		}
+	} else {
+		for i := 0; i < len(fileSave); i++ {
+			fmt.Print(fileSave[i] + " ")
+		}
 	}
+
 	for k := len(fileSave); k < *defaultNumber; k++ {
 		arrayToString := strings.Join(fileSave, " ")
+
 		for m := 0; m < len(save_txt); m++ {
 			save_txt_part = append(save_txt_part, save_txt[m])
 			if len(save_txt_part) == *prefix {
+				check_identically_word = true
 				for j := 0; j < *prefix; j++ {
-					if save_txt_part[j] == fileSave[j] {
-						save_identically_word++
+					if save_txt_part[j] != fileSave[j] {
+						check_identically_word = false
+						break
 					}
 				}
-				if save_identically_word == *prefix {
-					if m+1 < len(save_txt) {
-						wordMap[arrayToString] = append(wordMap[arrayToString], save_txt[m+1])
-					}
+				if check_identically_word && m+1 < len(save_txt) {
+					wordMap[arrayToString] = append(wordMap[arrayToString], save_txt[m+1])
 				}
-			} else if len(save_txt_part) > *prefix {
-				save_txt_part = save_txt_part[2:]
+				save_txt_part = save_txt_part[1:]
 			}
-			save_identically_word = 0
+
 		}
-		if len(wordMap[arrayToString]) == 0 {
-			fmt.Println("End")
-			os.Exit(1)
+		if len(wordMap[arrayToString]) > 0 {
+			randWordChoose = rand.Intn(len(wordMap[arrayToString]))
+			fmt.Print(wordMap[arrayToString][randWordChoose] + " ")
+			fileSave = append(fileSave[1:], wordMap[arrayToString][randWordChoose])
+			// fmt.Println(fileSave)
+		} else {
+			break
 		}
-		randWordChoose = rand.Intn(len(wordMap[arrayToString]))
-		fmt.Print(wordMap[arrayToString][randWordChoose] + " ")
-		fileSave = append(fileSave[1:], wordMap[arrayToString][randWordChoose])
-		// fmt.Println(fileSave)
 	}
 	fmt.Println()
 }
