eb8c2e097609a0443bba9a9d79bfa308d6461cb2
akakimbe <akakimbe@noreply.platform.alem.school>
2024-08-26 09:10:21 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,40 +7,48 @@ import (
 )
 
 var (
-	defaultNumber = flag.Int("default number", 100, "100")
-	help          = flag.Bool("help", false, "Da")
-	prefix        = flag.Int("default prefix", 2, "2")
+	defaultNumber = flag.Int("w", 100, "change default number")
+	help          = flag.Bool("help", false, "Check what you can do")
+	prefix        = flag.Int("l", 2, "change prefix")
+	prefixWord    = flag.String("p", "", "Where to start")
 )
 
 func main() {
-	var save_data []string
 	var save_txt []string
 	var fileSave []string
 	fileName := "the_great_gatsby.txt"
 
-	save_data = append(save_data, os.Args...)
-	fileSave = fileRead(fileName)
-	if len(save_data) >= 3 {
-		if save_data[1] == "-w" {
-			number := string_to_int(save_data[2])
-			*defaultNumber = number
-		} else {
-			fmt.Fprintln(os.Stderr, "Incorrect input")
-			os.Exit(1)
-		}
-		save_txt = fullFile()
-		mapSave(save_txt, fileSave)
-	} else {
-		fi, _ := os.Stdin.Stat()
-			if fi.Mode()&os.ModeNamedPipe == 0 {
-				fmt.Println("Error: no input text")
-				os.Exit(1)
-			}
-		if save_data[1] == "--help" {
-			*help = true
-		}
-		if *help {
-			helper()
-		}
+	flag.Parse()
+
+	if *help {
+		helper()
 	}
+
+	if *prefix != 2 {
+		checkPref(prefix)
+	}
+
+	if *defaultNumber != 100 {
+		checkNum(defaultNumber)
+	}
+
+	fi, _ := os.Stdin.Stat()
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		fmt.Println("Error: no input text")
+		os.Exit(1)
+	}
+
+
+	if *prefixWord != "" && *prefix != 0 {
+		checkLen()
+	}
+
+	if *prefixWord != "" {
+		checkWord(save_txt, fileSave)
+	} else {
+		save_txt = fullFile()
+		fileSave = fileRead(fileName)
+		mapSave(save_txt, fileSave)
+	}
+
 }
