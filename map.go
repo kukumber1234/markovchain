1f058f0e72944a32e802a63b095c59a9779ba562
akakimbe <akakimbe@noreply.platform.alem.school>
2024-08-26 10:30:59 +0000

    Update map.go

--- map.go
+++ map.go
@@ -9,8 +9,6 @@ import (
 
 func mapSave(save_txt []string, fileSave []string) {
 	wordMap := make(map[string][]string)
-	// var check_identically_word bool
-	// var save_txt_part []string
 	emptySlice := make([]string, *prefix)
 	var randWordChoose int
 
@@ -19,10 +17,7 @@ func mapSave(save_txt []string, fileSave []string) {
 		key := strings.Join(emptySlice, " ")
 		wordMap[key] = append(wordMap[key], save_txt[k])
 		newWord := save_txt[k]
-		// fmt.Println(wordMap[arrayToString][0])
 		if len(emptySlice) > 0 {
-			emptySlice = append(emptySlice[1:], newWord)
-		} else {
 			emptySlice = append(emptySlice[1:], newWord)
 		}
 	}
@@ -37,20 +32,16 @@ func mapSave(save_txt []string, fileSave []string) {
 		}
 	}
 
-	// fmt.Println(wordMap)
 	for i := 0; i < *defaultNumber-len(fileSave); i++ {
 		arrayToString := strings.Join(fileSave, " ")
 		if len(wordMap[arrayToString]) > 0 {
-			// fmt.Println(len(wordMap[arrayToString]))
 			randWordChoose = rand.Intn(len(wordMap[arrayToString]))
 			nextWord := wordMap[arrayToString][randWordChoose]
 			fmt.Print(nextWord + " ")
-			// fmt.Print(wordMap[arrayToString][randWordChoose] + " ")
 			fileSave = append(fileSave[1:], nextWord)
 		} else {
-			fmt.Println()
-			fmt.Println("It is the very last word in the text")
-			os.Exit(0)
+			fmt.Println(":this word do not exist or do not have continue in the text")
+			os.Exit(1)
 		}
 	}
 	fmt.Println()
